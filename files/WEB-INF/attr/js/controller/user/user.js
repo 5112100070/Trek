4212d3909cf68f807db8f3b9b81ee66c1b1b16ec
5112100070 <wahyu.kukuh.herlambang@gmail.com>
2018-10-04 23:54:02 +0700

    sweetalert for register user, reset password, etc

--- files/WEB-INF/attr/js/controller/user/user.js
+++ files/WEB-INF/attr/js/controller/user/user.js
@@ -5,37 +5,14 @@ function ProcessLogin(){
     promise = makeLogin(username, secret);
 
     promise.done(function(response){
-        swal({
-            title: "Login Berhasil",
-            icon: "success",
-            confirmButtonText: "OK",
-            customClass: "animated tada",
-            animation: false,
-        }).then(function(){
+        var swal = SimpleSwalWithoutDesc("Login Berhasil", type_success, "Ke Beranda");
+        swal.then(function(){
             window.location.href = base_url;
         });
     }).fail(function(response){
         $("#username").val("");
         $("#secret").val("");
-        if(response.status >= 500){
-            swal({
-                title: "Login Gagal",
-                text: "Terjadi kesalahan. Silahkan coba lagi",
-                icon: "error",
-                confirmButtonText: "Tutup",
-                customClass: "animated tada",
-                animation: false,
-              });
-        } else {
-            swal({
-                title: "Login Gagal",
-                text: "Username atau Password salah",
-                icon: "error",
-                confirmButtonText: "Tutup",
-                customClass: "animated tada",
-                animation: false,
-              });
-        }
+        SimpleSwal("Login Gagal", "Username atau Password salah", type_error, "Tutup");
     });
 }
 
@@ -51,8 +28,7 @@ function ProcessLogout(){
 
 function ProcessRegister(){
     if($("#password").val() != $("#password-ver").val()){
-        $("#request-alert-div").css("display", "block");
-        $("#request-alert").html("kesalahan pada verifikasi password");
+        SimpleSwal("Daftar Menjadi Anggota", "kesalahan pada verifikasi password", type_error, "Tutup");
         return;
     } 
 
@@ -61,31 +37,36 @@ function ProcessRegister(){
     password = $("#password").val();
 
     if(password.length <= 9){
-        $("#request-alert-div").css("display", "block");
-        $("#request-alert").html("Password kurang dari 10 karakter");
+        SimpleSwal("Daftar Menjadi Anggota", "Password kurang dari 10 karakter", type_error, "Tutup");
         return;
     }
 
     if(!validateEmail(email)){
-        $("#request-alert-div").css("display", "block");
-        $("#request-alert").html("Email tidak valid");
+        SimpleSwal("Daftar Menjadi Anggota", "Email tidak valid", type_error, "Tutup");
         return;
     }
 
     promise = registerUser(fullname, email, password);
     
-    promise.done(function(response){
-        window.location.href = base_url;
-    }).fail(function(response){
-        $("#username").val("");
-        $("#secret").val("");
-        if(response.status >= 500){
-            $("#request-alert-div").css("display", "block");
-            $("#request-alert").html("silahkan mencoba sekali lagi");
-        } else {
-            $("#request-alert-div").css("display", "block");
-            $("#request-alert").html(response.responseJSON.data.error_message);
-        }
+    var swal = SwalConfimationProcess("Daftar Menjadi Anggota", "Pastikan data yang akan anda daftarkan benar", type_warning, "Daftar!", "Batal");
+
+    swal.then(function(){
+        promise.done(function(response){
+            var swalDone = SimpleSwal("Daftar Menjadi Anggota", "Silahkan cek email untuk konfirmasi pendaftaran", type_success, "Pindah ke beranda");
+            swalDone.then(function(){
+                window.location.href = base_url;
+            });
+        }).fail(function(response){
+            $("#fullname").val("");
+            $("#email").val("");
+            $("#password").val("");
+            $("#password-ver").val("");
+            if(response.responseJSON != undefined){
+                SimpleSwal("Daftar Menjadi Anggota", response.responseJSON.data.error_message, type_error, "Tutup");                
+            } else {
+                SimpleSwal("Daftar Menjadi Anggota", "silahkan mencoba sekali lagi", type_error, "Tutup");
+            }
+        });
     });
     
 }
@@ -93,49 +74,22 @@ function ProcessRegister(){
 function ProcessResetPassword(){
     email = $("#email").val();
     if(!validateEmail(email)){
-        swal({
-            title: "Atur Ulang Password",
-            text: "Email tidak valid",
-            icon: "error",
-            confirmButtonText: "Tutup",
-            customClass: "animated tada",
-            animation: false,
-          });
-          return;
+        SimpleSwal("Atur Ulang Password", "Email tidak valid", type_error, "Tutup");
+        return;
     }
 
     promise = resetPasssword(email);
     promise.done(function(response){
-        swal({
-            title: "Atur Ulang Password",
-            text: "Sukses mengatur ulang passsword. Silahkan cek email",
-            icon: "success",
-            confirmButtonText: "Kembali ke halaman utama",
-            customClass: "animated tada",
-            animation: false,
-          }).then(function(){
+        var swal = SimpleSwal("Atur Ulang Password", "Sukses mengatur ulang passsword. Silahkan cek email", type_success, "Kembali ke halaman utama");
+        swal.then(function(){
             window.location.href = base_url;
-          });
+        });
     }).fail(function(response){
         $("#email").val("");
         if(response.status >= 500){
-            swal({
-                title: "Atur Ulang Password",
-                text: "Terjadi kesalahan. Silahkan coba lagi",
-                icon: "error",
-                confirmButtonText: "Tutup",
-                customClass: "animated tada",
-                animation: false,
-              });
+            SimpleSwal("Atur Ulang Password", "Terjadi kesalahan. Silahkan coba lagi. Silahkan cek email", type_error, "Tutup");
         } else {
-            swal({
-                title: "Atur Ulang Password",
-                text: response.responseJSON.data.error_message,
-                icon: "error",
-                confirmButtonText: "Tutup",
-                customClass: "animated tada",
-                animation: false,
-              });
+            SimpleSwal("Atur Ulang Password", response.responseJSON.data.error_message, type_error, "Tutup");
         }
     });
     
---
+++ files/WEB-INF/attr/js/sweetalert-main.js
@@ -0,0 +1,63 @@
+const type_success = "success";
+const type_error = "error";
+const type_question = "question";
+const type_warning = "warning";
+
+function SimpleSwalWithoutDesc(title, type, confirmButton){
+    return swal({
+        title: title,
+        type: type,
+        confirmButtonText: confirmButton,
+        customClass: "animated tada",
+        animation: false,
+        imageUrl: '/img/logo-trek.png',
+        imageWidth: 150,
+        imageHeight: 50,
+        background: 'rgb(255, 204, 0)',
+        imageAlt: 'Trek Logo',
+    });
+}
+
+function SimpleSwal(title, text, type, confirmButton){
+    return swal({
+        title: title,
+        text: text,
+        type: type,
+        confirmButtonText: confirmButton,
+        customClass: "animated tada",
+        animation: false,
+        imageUrl: '/img/logo-trek.png',
+        imageWidth: 150,
+        imageHeight: 50,
+        background: 'rgb(255, 204, 0)',
+        imageAlt: 'Trek Logo',
+    });
+}
+
+function SwalConfimationProcess(title, question, type, confirmButton, cancelButton){
+    return swal({
+        title: title,
+        text: question,
+        type: type,
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: confirmButton,
+        cancelButtonText: cancelButton,
+        customClass: "animated tada",
+        animation: false,
+        imageUrl: '/img/logo-trek.png',
+        imageWidth: 150,
+        imageHeight: 50,
+        background: 'rgb(255, 204, 0)',
+        imageAlt: 'Trek Logo',
+      });
+}
+
+function validateType(type){
+    if(type==type_success || type==type_error || type==type_question || type == type_warning){
+        return type;
+    } else {
+        return type_success;
+    }
+}
